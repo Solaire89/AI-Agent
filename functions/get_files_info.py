94205fa829e0a81755db7a8177f0abb033d394f7
ThePrimeagen <the.primeagen@aol.com>
2025-07-13 17:26:35 -0500

    Rearranged the contents to the right spot

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,19 +1,21 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    absolute_working_directory = os.path.abspath(working_directory) # Getting the absolute path of the working_directory
-    absolute_directory = os.path.abspath(directory) # Getting the absolute path of the directory
-    if not absolute_directory.startswith(absolute_working_directory): # CHecking if the absolute directory doesn't start with the absolute working directory
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory' # Return the error message if it doesn't
-    if not os.path.isdir(absolute_directory): # Checking if the directory isn't a directory
-        return f'Error: {directory} is not a directory' # Returning the error message
-    if directory is None: # Checking if the directory is None
-        directory = "." # If it is, setting the directory to '.'
-    full_directory = os.path.join(absolute_working_directory, directory) # Concatenating the working directory and directory to get the full absolute value
-    item_names = os.listdir(full_directory) # Create a list of files/directories in the current directory
-    item_list = [] # Creating a list of directory items to append the objects to in the following for loop
-    for item in item_names:
-        if os.path.isdir(item): 
-            full_path = os.path.join(absolute_working_directory, item)
-            item_list.append(f"{item}: file size={os.path.getsize(full_path)}, is_dir={os.path.isdir(full_path)}")
-    return "/n".join(item_list)
+    absolute_working_directory = os.path.abspath(working_directory) 
+    target_directory = absolute_working_directory
+    if directory:
+        target_directory = os.path.abspath(os.path.join(working_directory, directory))
+    if not target_directory.startswith(absolute_working_directory): 
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory' 
+    if not os.path.isdir(target_directory):
+        return f'Error: {directory} is not a directory' 
+    try:
+        files_info = [] 
+        for filename in os.listdir(target_directory):
+            full_path = os.path.join(target_directory, filename)
+            files_info.append(
+                f"- {filename}: file size={os.path.getsize(full_path)}, is_dir={os.path.isdir(full_path)}"
+                )
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error listing files: {e}"
--- tests.py
+++ tests.py
@@ -6,5 +6,20 @@ def test():
     print(result)
     print("")
 
+    result = get_files_info("calculator", "pkg")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "/bin")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "../")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
 if __name__ == "__main__":
     test()
