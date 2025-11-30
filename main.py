92370f67787e6fda83e5fd510329ba68a8184ee0
ThePrimeagen <the.primeagen@aol.com>
2025-11-30 17:23:39 -0600

    Uploading AI agent to the github site

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
+from prompts import system_prompt
 from dotenv import load_dotenv # type: ignore
 from google import genai
 from google.genai import types # type: ignore
+from functions.get_files_info import available_functions, schema_get_files_info
 
 def main():
     load_dotenv()
@@ -17,18 +19,22 @@ def main():
     user_prompt = sys.argv[1]
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
-    
-
-    
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+    # response is a variable containing the object as a result of generate_content
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], 
+            system_instruction=system_prompt
+            )
     )
+
+    for function_call in response.function_calls:
+        f"Calling function: {response.function_call.name}({response.function_call.args})"
 
     print(response.text)
     if "--verbose" in cmd_input:
